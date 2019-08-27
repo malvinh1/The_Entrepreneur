d03ac3d991cb6c9844ba389398cd74918db55b3e
Alessandro Ferdian <alessandroferdian128@gmail.com>
2019-08-27 13:58:41 +0700

    Tes Pull Request

--- front-end/src/scenes/SignInScene.tsx
+++ front-end/src/scenes/SignInScene.tsx
@@ -8,7 +8,7 @@ export default class SignInScene extends Component<Props, State> {
   render() {
     return (
       <View style={styles.container}>
-        <Text> This is Sign In Scene!</Text>
+        <Text> This is Sign In Scenes !</Text>
       </View>
     );
   }
